39f8e57dd85ba2f876f50211fb22a8082c5ddfdc
Min-Hsiu Hsu <mhhsu2@illinois.edu>
2021-05-05 12:54:09 -0500

    Replace jquery datatable with dash datatable

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import dash
+import dash_table
 from flask import Flask, redirect, render_template, request, session, url_for
 from flask_login import (
     LoginManager,
@@ -29,6 +31,27 @@ def index():
 def data(team):
     db = Database()
     data, col_name_trans = db.sprjoint_table_view()
+    dashapp.layout = dash_table.DataTable(
+        id="table",
+        columns=[{"name": dis, "id": db} for db, dis in col_name_trans.items()],
+        data=data,
+        style_data_conditional=[
+            {"if": {"row_index": "odd"}, "backgroundColor": "rgb(248, 248, 248)"}
+        ],
+        style_header={"backgroundColor": "rgb(230, 230, 230)", "fontWeight": "bold"},
+        style_cell={
+            # all three widths are needed
+            "minWidth": "240px",
+            "width": "240px",
+            "maxWidth": "240px",
+        },
+        editable=True,
+        sort_action="native",
+        filter_action="native",
+        page_action="native",
+        export_format="xlsx",
+        export_headers="display",
+    )
 
     if request.method == "POST":
         formdata = request.form.to_dict()
@@ -48,6 +71,39 @@ def data(team):
         return redirect(url_for("data", team=team))
 
     return render_template("data.html", data=data, col_name_trans=col_name_trans, team=team)
+
+
+# Dashapp
+db = Database()
+d, col_name_trans = db.sprjoint_table_view()
+dashapp = dash.Dash(
+    __name__, server=app, url_base_pathname="/dashapp/", assets_folder="static/dashassets"
+)
+
+dashapp.layout = dash_table.DataTable(
+    id="table",
+    columns=[{"name": dis, "id": db} for db, dis in col_name_trans.items()],
+    data=d,
+    style_data_conditional=[{"if": {"row_index": "odd"}, "backgroundColor": "rgb(248, 248, 248)"}],
+    style_header={"backgroundColor": "rgb(230, 230, 230)", "fontWeight": "bold"},
+    style_cell={
+        # all three widths are needed
+        "minWidth": "240px",
+        "width": "240px",
+        "maxWidth": "240px",
+    },
+    editable=True,
+    sort_action="native",
+    filter_action="native",
+    page_action="native",
+    export_format="xlsx",
+    export_headers="display",
+)
+
+
+@app.route("/dashapp")
+def dash_app():
+    return dashapp.index()
 
 
 if __name__ == "__main__":
