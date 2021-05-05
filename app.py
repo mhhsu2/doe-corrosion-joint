af650fed944c75178dbe35d6864a10c4357862d3
Min-Hsiu Hsu <mhhsu2@illinois.edu>
2021-05-05 13:29:20 -0500

    Reactor dash datatable

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import dash
-import dash_table
 from flask import Flask, redirect, render_template, request, session, url_for
 from flask_login import (
     LoginManager,
@@ -11,6 +10,7 @@ from flask_login import (
     logout_user,
 )
 
+from datatable import create_datatable
 from db import Database
 
 app = Flask(__name__)
@@ -19,6 +19,13 @@ app.config["SECRET_KEY"] = "doe-corrosion-joint"
 # login_manager = LoginManager()
 # login_manager.init_app(app)
 # login_manager.login_view = "login"
+
+# Dashapp
+dashapp = dash.Dash(
+    __name__, server=app, url_base_pathname="/dashapp/", assets_folder="static/dashassets"
+)
+
+dashapp.layout = create_datatable(data=[{"init": "test"}])
 
 
 @app.route("/", methods=["GET"])
@@ -31,28 +38,7 @@ def index():
 def data(team):
     db = Database()
     data, col_name_trans = db.sprjoint_table_view()
-    dashapp.layout = dash_table.DataTable(
-        id="table",
-        columns=[{"name": dis, "id": db} for db, dis in col_name_trans.items()],
-        data=data,
-        style_data_conditional=[
-            {"if": {"row_index": "odd"}, "backgroundColor": "rgb(248, 248, 248)"}
-        ],
-        style_header={"backgroundColor": "rgb(230, 230, 230)", "fontWeight": "bold"},
-        style_cell={
-            # all three widths are needed
-            "minWidth": "240px",
-            "width": "240px",
-            "maxWidth": "240px",
-        },
-        editable=True,
-        sort_action="native",
-        filter_action="native",
-        page_action="native",
-        page_size=25,
-        export_format="xlsx",
-        export_headers="display",
-    )
+    dashapp.layout = create_datatable(data, col_name_trans)
 
     if request.method == "POST":
         formdata = request.form.to_dict()
@@ -72,36 +58,6 @@ def data(team):
         return redirect(url_for("data", team=team))
 
     return render_template("data.html", data=data, col_name_trans=col_name_trans, team=team)
-
-
-# Dashapp
-db = Database()
-d, col_name_trans = db.sprjoint_table_view()
-dashapp = dash.Dash(
-    __name__, server=app, url_base_pathname="/dashapp/", assets_folder="static/dashassets"
-)
-
-dashapp.layout = dash_table.DataTable(
-    id="table",
-    columns=[{"name": dis, "id": db} for db, dis in col_name_trans.items()],
-    data=d,
-    style_data_conditional=[{"if": {"row_index": "odd"}, "backgroundColor": "rgb(248, 248, 248)"}],
-    style_header={"backgroundColor": "rgb(230, 230, 230)", "fontWeight": "bold"},
-    style_cell={
-        # all three widths are needed
-        "minWidth": "240px",
-        "width": "240px",
-        "maxWidth": "240px",
-    },
-    style_table={"overflowX": "auto"},
-    editable=True,
-    sort_action="native",
-    filter_action="native",
-    page_action="native",
-    page_size=25,
-    export_format="xlsx",
-    export_headers="display",
-)
 
 
 @app.route("/dashapp")
--- datatable.py
+++ datatable.py
@@ -3,31 +3,42 @@ import dash_table
 
 from db import Database
 
-db = Database()
-data, col_name_trans = db.sprjoint_table_view()
-# print(data)
 
-app = dash.Dash(__name__, assets_folder="static/dashassets")
+def create_datatable(data: dict, col_name_trans: dict = None) -> dash_table.DataTable:
+    if col_name_trans is not None:
+        columns = [{"name": dis, "id": db} for db, dis in col_name_trans.items()]
+    else:
+        columns = [{"name": k, "id": k} for k in data[0].keys()]
 
-app.layout = dash_table.DataTable(
-    id="table",
-    columns=[{"name": dis, "id": db} for db, dis in col_name_trans.items()],
-    data=data,
-    style_data_conditional=[{"if": {"row_index": "odd"}, "backgroundColor": "rgb(248, 248, 248)"}],
-    style_header={"backgroundColor": "rgb(230, 230, 230)", "fontWeight": "bold"},
-    style_cell={
-        # all three widths are needed
-        "minWidth": "240px",
-        "width": "240px",
-        "maxWidth": "240px",
-    },
-    editable=True,
-    sort_action="native",
-    filter_action="native",
-    page_action="native",
-    export_format="xlsx",
-    export_headers="display",
-)
+    return dash_table.DataTable(
+        id="table",
+        columns=columns,
+        data=data,
+        style_data_conditional=[
+            {"if": {"row_index": "odd"}, "backgroundColor": "rgb(248, 248, 248)"}
+        ],
+        style_header={"backgroundColor": "rgb(230, 230, 230)", "fontWeight": "bold"},
+        style_cell={
+            # all three widths are needed
+            "minWidth": "240px",
+            "width": "240px",
+            "maxWidth": "240px",
+        },
+        editable=True,
+        sort_action="native",
+        filter_action="native",
+        page_action="native",
+        page_size=25,
+        export_format="xlsx",
+        export_headers="display",
+    )
+
 
 if __name__ == "__main__":
+    db = Database()
+    data, col_name_trans = db.sprjoint_table_view()
+
+    app = dash.Dash(__name__, assets_folder="static/dashassets")
+    app.layout = create_datatable(data, col_name_trans)
+    
     app.run_server(debug=True)
